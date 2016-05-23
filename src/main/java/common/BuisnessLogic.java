bba8728bf79b88a790c9149f5ee76d4193c142f6
san3133 <irfannadir@ymail.com>
2016-05-23 20:59:38 +0530

    testing basepath

--- src/main/java/common/BuisnessLogic.java
+++ src/main/java/common/BuisnessLogic.java
@@ -38,7 +38,12 @@ public class BuisnessLogic {
 	
 				
 //String jsonBody = u.generateStringFromResource("/Users/irfannadir/Documents/workspace/RestAssured_Furlenco/src/main/java/properties/testOrders1.json");
-	String jsonBody = u.generateStringFromResource("/home/travis/build/san3133/ApiTesting_Furlenco/src/main/java/properties/testOrders1.json");
+	//String jsonBody = u.generateStringFromResource("/home/travis/build/san3133/ApiTesting_Furlenco/src/main/java/properties/testOrders1.json");
+File basepath = new File(System.getProperty("user.dir"));
+
+		
+		String jsonBody =  u.generateStringFromResource(basepath +"/src/main/java/properties/testOrders1.json");
+
 
 
 				String content = null;
